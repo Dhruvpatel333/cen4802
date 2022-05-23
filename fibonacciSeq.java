8bad7c9de82642e1739c65560ad37b23218b0813
Dhruvpatel333 <96754329+Dhruvpatel333@users.noreply.github.com>
2022-05-22 20:44:35 -0400

    Add files via upload

--- fibonacciSeq.java
+++ fibonacciSeq.java
@@ -1,10 +1,20 @@
 package fibSeqeunceRecursive;
 
+
 import java.util.Scanner;
 import java.util.*;
 
+/**
+ * @author Dhruv
+ *
+ * In this assignment an recursive function is used to show the term at the nth position in a Fibonacci Sequence
+ */
 public class fibonacciSeq{
 
+		/**
+		 * @param n
+		 * @return This will return the result of the sequence after reaching the nth term.
+		 */
 		public static int fib(int n) {
 			if(n ==0 )
 				return n;
@@ -16,6 +26,7 @@ public class fibonacciSeq{
 				
 		}
 		
+
 		public static void main(String[] args){
 			
 			Scanner input = new Scanner(System.in);
